e869329097b021751a1915abb4b1ef3d4265006e
Salvador Sandoval <developer5.vwd@gmail.com>
2026-04-25 22:34:02 -0600

    feat(usuario): modulo de registro, olvido de contraseña e inicio de sesion implementado

--- desarrollo/gestion-ets-auth-api/src/main/java/com/gestion/ets/api/core/enums/RolesEnum.java
+++ desarrollo/gestion-ets-auth-api/src/main/java/com/gestion/ets/api/core/enums/RolesEnum.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RolesEnum {
-    ALUMNO(1,"Administrador");
+    ALUMNO(1,"Alumno");
     private Integer id;
     private String nombre;
 }
