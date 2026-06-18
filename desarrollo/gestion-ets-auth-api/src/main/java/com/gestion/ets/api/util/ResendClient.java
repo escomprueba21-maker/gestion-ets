4cd1b912e1a050c9abd238062db077acfd8f9c08
Salvador Sandoval <developer5.vwd@gmail.com>
2026-06-18 15:23:59 -0600

    feat: urls apis

--- desarrollo/gestion-ets-auth-api/src/main/java/com/gestion/ets/api/util/ResendClient.java
+++ desarrollo/gestion-ets-auth-api/src/main/java/com/gestion/ets/api/util/ResendClient.java
@@ -1,15 +1,23 @@
 package com.gestion.ets.api.util;
+
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
 @RegisterRestClient(configKey = "resend-api")
-@ClientHeaderParam(name = "Authorization", value = "Bearer ${resend.api.key}")
 @Path("/emails")
 public interface ResendClient {
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
+    @ClientHeaderParam(name = "Authorization", value = "{com.gestion.ets.api.util.ResendClient.getAuthHeader}")
     void send(ResendEmailRequest request);
+
+    default String getAuthHeader() {
+        return "Bearer " + org.eclipse.microprofile.config.ConfigProvider.getConfig()
+                .getValue("resend.api.key", String.class);
+    }
 }
